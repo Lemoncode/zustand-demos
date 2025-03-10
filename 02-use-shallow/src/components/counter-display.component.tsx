461fe090e8b13762ecdef82b7bc87f60facb8226
Braulio <braulio.diez@gmail.com>
2025-03-10 21:22:56 +0100

    useShallow

---
+++ 02-use-shallow/src/components/counter-display.component.tsx
@@ -0,0 +1,24 @@
+import { useShallow } from "zustand/shallow";
+import { useCounter } from "../stores/counter.store";
+
+export function CounterDisplay() {
+  //const { count, id } = useCounter();
+
+  //const id = useCounter((state) => state.id);
+  //const count = useCounter((state) => state.count);
+
+  const { count, id } = useCounter(
+    useShallow((state) => ({
+      count: state.count,
+      id: state.id,
+    }))
+  );
+
+  console.log(">> CounterDisplay render");
+
+  return (
+    <h2>
+      {id} Current value: {count}
+    </h2>
+  );
+}
