6306674b35ca369d8d6dfd0467072a40fa93c687
Braulio <braulio.diez@gmail.com>
2025-03-10 14:51:15 +0100

    ocmpleted

--- 01-shallow-compare/src/components/counter-display.component.tsx
+++ 01-shallow-compare/src/components/counter-display.component.tsx
@@ -2,6 +2,5 @@ import { useCounter } from "../stores/counter.store";
 
 export function CounterDisplay() {
   const { count } = useCounter();
-  console.log("CounterDisplay rerender");
   return <h2>Current value: {count}</h2>;
 }
--- 01-shallow-compare/src/components/counter-increment.component.tsx
+++ 01-shallow-compare/src/components/counter-increment.component.tsx
@@ -1,12 +1,8 @@
-import { useShallow } from "zustand/shallow";
 import { useCounter } from "../stores/counter.store";
 
 export function CounterIncrement() {
-  const { inc } = useCounter(
-    useShallow((state) => ({
-      inc: state.inc,
-    }))
-  );
+  const inc = useCounter((state) => state.inc);
+
   console.log("CounterIncrement render");
   return <button onClick={inc}>Increment</button>;
 }
